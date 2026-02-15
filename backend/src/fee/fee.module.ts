31d4633a95dfaa1f6e95ea91240ec097e301a426
caxtonacollins <caxtonacollins@gmail.com>
2026-02-15 13:12:48 +0100

    fix: add missing module dependencies to FeeModule

--- backend/src/fee/fee.module.ts
+++ backend/src/fee/fee.module.ts
@@ -5,9 +5,16 @@ import { FeeService } from './fee.service';
 import { FeeEstimate } from '../entities/fee-estimate.entity';
 import { NetworkStatus } from '../entities/network-status.entity';
 
+import { StacksModule } from '../stacks/stacks.module';
+import { RedisModule } from '../redis/redis.module';
+import { FeeOracleModule } from '../contracts/fee-oracle/fee-oracle.module';
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([FeeEstimate, NetworkStatus]),
+    StacksModule,
+    RedisModule,
+    FeeOracleModule,
   ],
   controllers: [FeeController],
   providers: [FeeService],
