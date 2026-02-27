fbb0257c189d77c67ba88b04be3d1e858fcdacc4
caxtonacollins <caxtonacollins@gmail.com>
2026-02-27 23:04:37 +0100

    feat(frontend): create hooks index to export all custom hooks

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { useCurrentFee } from './useCurrentFee';
+export { useEstimate } from './useEstimate';
+export { useFeeHistory } from './useFeeHistory';
+export { useWallet } from './useWallet';
