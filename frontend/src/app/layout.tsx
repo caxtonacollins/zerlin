af5933dcc1541d84b3e30d971a8e4d91f846fa77
caxtonacollins <caxtonacollins@gmail.com>
2026-03-07 00:19:18 +0100

    style(frontend): update app layout and metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -33,6 +33,10 @@ export default function RootLayout({
           content="5464c165f883df4cb51c6fc03b88eeb160f53c4acd8b20220efd210fb93629c6b6e302bae8478f171f817a33c6cab2b815f9a8b058ac32d74404a74c31474103"
         />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
